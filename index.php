31c3f0cc3e6505e2a1d297b0aa232eaeaf6ea0e1
davimouravilaca <davimouravilaca@gmail.com>
2023-11-10 20:18:26 -0300

    project start

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $title = null;
+
+    $img1 = "https://placehold.co/600x400/orange/white?text=Product+Sample+1";
+    $img2 = "https://placehold.co/600x400/cyan/white?text=Product+Sample+2";
+    $img3 = "https://placehold.co/600x400/pink/white?text=Product+Sample+3";
+
+    $img = [$img1, $img2, $img3];
+
+    require "public/includes/header.php";
+
+
+?>
+
+<!-- Content goes here -->    
+
+    <h1 class="display-1">Hello World</h1>
+
+    <?php include "public/includes/carousel.php" ?>
+
+<?php require "public/includes/footer.php" ?>
---
+++ public/includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?=$title ?? "Star Shop"?> </title>
+
+    <link rel="shortcut icon" href="https://icons.iconarchive.com/icons/arturo-wibawa/akar/256/rock-on-icon.png" type="image/x-icon">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href= "public/resources/css/style.css">
+</head>
+<body>
+
+    <!-- Nav-bar-->
+
+    <?php include "navbar.php"?>
+
+    <div class="container-fluid"> 
+
+    <!-- Content -->
